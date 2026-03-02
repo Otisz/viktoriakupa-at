11f1f41412240df2e916a0fa7ee032a4be38f7f3
Levente Otta <leventeotta@gmail.com>
2026-03-02 10:44:20 +0100

    Fix

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/rolunk', [HomeController::class, 'aboutUs'])->name('about-us');
 Route::get('/egyesuletek', [HomeController::class, 'clubs'])->name('clubs');
 Route::get('/szabalyzatok', [HomeController::class, 'rules'])->name('rules');
+Route::get('/versenykiirasok', [HomeController::class, 'competitions'])->name('competitions');
 Route::get('/versenynaptar', [HomeController::class, 'calendar'])->name('calendar');
 Route::get('/kapcsolat', [HomeController::class, 'contact'])->name('contact');
 Route::get('/hirek', [PostController::class, 'index'])->name('posts.index');
