733e70b3b0a330019791c94f4bbda30a766059e1
Levente Otta <leventeotta@gmail.com>
2026-06-26 10:08:08 +0200

    Change header items order

--- resources/js/components/header/index.tsx
+++ resources/js/components/header/index.tsx
@@ -8,14 +8,14 @@ export default function Header() {
           <img
             src="/images/logo_matsz.png"
             alt="Magyar Akrobatikus Torna Szakág logo"
-            className="h-26 w-auto order-1"
+            className="h-26 w-auto order-2 lg:order-3"
           />
           <div className="flex flex-col gap-2 items-center justify-center text-primary-foreground tracking-wider text-center text-balance order-3 lg:order-2">
             <p className="font-semibold text-xl">Magyar Torna Szövetség</p>
             <p className="font-semibold text-2xl">Akrobatikus Torna Szakág</p>
             <p className="font-bold text-3xl">Viktoria Kupa Versenysorozat</p>
           </div>
-          <img src="/images/logo_vk.png" alt="Viktoria Kupa logo" className="h-26 w-auto order-2 lg:order-3" />
+          <img src="/images/logo_vk.png" alt="Viktoria Kupa logo" className="h-26 w-auto order-1" />
         </Link>
       </div>
     </header>
